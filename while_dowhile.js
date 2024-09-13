f01af3e5b99f162d6df8127a6550baea4fc419a5
Hamza Khan <hamza.khan951hk@gmail.com>
2024-09-13 19:07:28 +0500

    while and do_while

---
+++ while_dowhile.js
@@ -0,0 +1,16 @@
+// While and do while loop
+let myArray = ["Ironman", "Loki", "Spiderman"];
+let arr = 0;
+while (arr < myArray.length) {
+  //   console.log(`Value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 1; //agar 11 ho to ya 11 ko print kar ga
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
+
+//Do while loop pahla code run hota ha phir condition check hoti ha
